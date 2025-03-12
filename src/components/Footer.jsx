17a6691df266c1ab27d264d086f94b86a40bbb34
Rodoll99 <dudtjr7137@naver.com>
2025-03-12 18:47:56 +0900

    lecture

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 const Footer = () => {
     return(
+        //jsx로 인식하여 에러가 나지않는다.
         <footer>
             <h1>footer</h1>
         </footer>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,13 @@
 const Main = () => {
+    // const number = 9;
+
+    // JSX주의 사항
+    // 1. 중괄호 내부에는 자바스크립트 표현식만 넣을 수 있다.( if 문 , for 문 사용 불가 )
+    // 2. 
     return (
     <main>
         <h1>main</h1>
+        {/* <h2>{number % 2 ===0 ? "짝수" : "홀수" }</h2> */}
     </main>
     )
 }
