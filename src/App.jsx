950221ba272f5ee2a3436af4f51c830bf8ae8562
Rodoll99 <dudtjr7137@naver.com>
2025-03-18 17:34:23 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import './App.css'
 import { useState } from 'react';
 
+const Bulbe = ({light}) => {
+  return (
+  <div>{light === "ON" ? (<h1 style={{backgroundColor : 'orange'}}>ON</h1>) : (
+    <h1 style={{backgroundColor : 'gray'}}>OFF</h1>
+  )}</div>)
+}
 function App() {
   // 일반적 함수 
   // [A,B] = UseState(기본값)
-  // A는 변수 설정 , B는 A의 상태를 변화시킬 함수 설정
+  // A는 변수(State) 설정 , B는 A의 상태를 변화시킬 함수 설정
+
+  //props는 컴포넌트에 전달되는 부모데이터 , state는 컴포넌트 안에서 관리되는 데이터
   const [count, setCount] = useState(0);
   const [light , setLight] = useState("OFF")
   return (
     // 컴포넌트의 집합
     <>
     <div>
+      {/* 컴포넌트에 props 전달 */}
+    <Bulbe light={light}></Bulbe>
     <h1>{count}</h1>
       <button onClick= {()=>{
         setCount(count + 1)
