2d19486cbe720e1e7a83aaabee1b8b529965df62
Rodoll99 <dudtjr7137@naver.com>
2025-03-31 15:21:08 +0900

    Update Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -7,37 +7,65 @@ import { useState } from "react";
 
 const Register = () => {
     //[변수 , 함수 ] = useState(기본값) 
-    const [name , setName] = useState("이름");
-    const [birth , setBirth] = useState("")
-    const [country , setCountry] =useState("")
-    const [bio , setBio] = useState("")
+        // const [name , setName] = useState("이름");
+        // const [birth , setBirth] = useState("")
+        // const [country , setCountry] =useState("")
+        // const [bio , setBio] = useState("")
+        const [input , setInput] =useState({
+            name:"" ,
+            birth:"",
+            country:"",
+            bio:"",
+        })
+    console.log(input)
     // 매개변수(e)를 넘김
-    const onChangeName = (e) => {
-        // 매개변수의 target.value 에 입력된값이 저장되기때문
-        setName(e.target.value);
+    const onChange = (e) => {
+        setInput({
+            ...input,
+            [e.target.name]:e.target.value,
+        });
     }
-    const onChageBirth = (e) => {
-        setBirth(e.target.value);
-    }
-    const onChangeCountry = (e) => {
-        setCountry(e.target.value);
-    } 
-    const onChangeBio = (e)=>{
-        setBio(e.target.value);
-    }
+    // const onChangeName = (e) => {
+    //     // 매개변수의 target.value 에 입력된값이 저장되기때문
+    //     // setName(e.target.value);
+    //     setInput({
+    //         ...input , 
+    //         name : e.target.value ,
+    //     });
+    // }
+    // const onChageBirth = (e) => {
+    //     // setBirth(e.target.value);
+    //     setInput({
+    //         ...input , 
+    //         birth : e.target.value ,
+    //     });
+    // }
+    // const onChangeCountry = (e) => {
+    //     // setCountry(e.target.value);
+    //     setInput({
+    //         ...input , 
+    //         country : e.target.value ,
+    //     });
+    // } 
+    // const onChangeBio = (e)=>{
+    //     setInput({
+    //         ...input , 
+    //         bio : e.target.value ,
+    //     });
+    // }
     return (
         <div>
             <div>
                 {/* onChange로 입력된 값을 받고 onChangeName으로 값을 전달함 */}
-                <input value={name} onChange={onChangeName} placeholder="이름" />
+                <input name="name" value={input.name} onChange={onChange} placeholder="이름" />
                 </div>
             <div>
                 {/* date = 날짜를 입력받는 함수 */}
-                <input value={birth} type="date" onChange={onChageBirth}></input>
+                <input name="birth" value={input.birth} type="date" onChange={onChange}></input>
             </div>
             <div>
                 {/* select , option */}
-               <select value={country} onChange={onChangeCountry}>
+               <select name ="country"value={input.country} onChange={onChange}>
                     <option value="KR">
                         한국
                     </option>
@@ -51,9 +79,9 @@ const Register = () => {
             </div>
             <div>
                 {/* textarea = 글자입력 폼 */}
-                <textarea onChange={onChangeBio} value={bio}>
+                <textarea name="bio"onChange={onChange} value={input.bio}>
                 </textarea>
-                {bio}
+    
             </div>
         </div>
       
